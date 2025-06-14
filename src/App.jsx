d236d6f7fac01be2f7178bfecd28b7b508023249
tato <email.tato@gmail.com>
2025-06-13 21:42:29 -0300

    ambiente config

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import './App.css'
 function App() {
 
   return (
-    <div className=''>
-      
+    <div className='flex'>
+      <div className="bg-salmao w-[10rem] h-[10rem] block "></div>
+      <div className="bg-roxo w-[10rem] h-[10rem] block "></div>
+      <div className="bg-bege w-[10rem] h-[10rem] block "></div>
+      <h1 className='font-inter text-9xl text-salmao'>Font: Inter</h1>
     </div>
   )
 }
