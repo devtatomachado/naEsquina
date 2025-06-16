ebfa1380f7ee9ce63f08972fd83f66e83ad9ec95
tato <email.tato@gmail.com>
2025-06-16 11:23:11 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className='flex'>
-      <div className="bg-salmao w-[10rem] h-[10rem] block "></div>
-      <div className="bg-roxo w-[10rem] h-[10rem] block "></div>
-      <div className="bg-bege w-[10rem] h-[10rem] block "></div>
-      <h1 className='font-inter text-9xl text-salmao'>Font: Inter</h1>
+      <Header />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 function Header() {
     return (
-        <header>
-            <h1>Header</h1>
+        <header className='w-full flex justify-between py-5 px-12 bg-salmao'>
+            <Link to="/">
+                <img
+                    src="./iconeNaEsquina.svg"
+                    alt="Icone NaEsquina"
+                    className='w-12'
+                />
+            </Link>
+            <form>
+                <div>
+                    <input type="text" />
+                    <button> 
+
+                    </button>
+                </div>
+            </form>
+            <button>Entrar</button>
         </header>
     );
 }
