38ab7f353c7e8968c3c0b855cc34881c4e8698e9
Renanpacheco <prenan86@gmail.com>
2025-06-19 21:32:56 -0300

    cadastro de pessoa

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import AdicionarProdutos from './pages/AdicionarProdutos.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import AdicionarProdutos from "./pages/AdicionarProdutos.jsx";
+import CadastroPessoa from "./pages/CadastroPessoa.jsx";
 
 const rotas = createBrowserRouter([
-  
-  {path:"/", element: <Home/>},
-  {path:"/Login", element: <Login/>}
-  {path:"/", element: <Home/>},
-  {path:"/adicionar", element: <AdicionarProdutos/>}
+  { path: "/", element: <Home /> },
+  { path: "/Login", element: <Login /> },
+  { path: "/", element: <Home /> },
+  { path: "/adicionar", element: <AdicionarProdutos /> },
+  { path: "/cadastro", element: <CadastroPessoa /> },
+]);
 
-])
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={rotas} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
