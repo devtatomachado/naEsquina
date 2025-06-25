f2ad60b681d987b98ab6ddd5bedcf03c614cad5b
tato <email.tato@gmail.com>
2025-06-25 14:37:17 -0300

    up

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,9 +4,11 @@ function Banner() {
     return (
         <section className='bg-roxo flex md:flex-row flex-col justify-between md:gap-0 gap-4 items-center md:rounded-xl py-5 md:px-12'>
             <img src="./shop.png" alt="Foto entrega de compra" className='w-[13%] hidden md:block' />
-            <div className='flex flex-col justify-center items-start gap-4'>
-                <h1 className='text-salmao text-2xl md:text-5xl text-wrap font-bold text-center md:text-left '>Multiplique sua visibilidade com o <span className='text-branco'>NaEsquina</span></h1>
-                <h2 className='text-branco text-xl font-light md:block hidden'>Divulgue com facilidade em uma plataforma poderosa e intuitiva</h2>
+            <div className="flex justify-center">
+                <div className='flex flex-col justify-center items-start gap-4'>
+                    <h1 className='text-salmao text-2xl md:text-5xl text-wrap font-bold text-center md:text-left '>Multiplique sua visibilidade com o <span className='text-branco'>NaEsquina</span></h1>
+                    <h2 className='text-branco text-xl font-light md:block hidden'>Divulgue com facilidade em uma plataforma poderosa e intuitiva</h2>
+                </div>
             </div>
             <Link to="/"><p className='bg-salmao py-3 px-5  md:text-sm rounded-xl text-roxo text-nowrap font-bold hover:brightness-110 duration-300 ease-in-out '>Cadastre-se</p></Link>
         </section>
